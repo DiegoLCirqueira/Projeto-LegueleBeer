5d6999c97ab005e61ced8a66c8c1b900656a5137
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-01-25 11:52:47 -0300

    att

--- projeto/script.js
+++ projeto/script.js
@@ -5,12 +5,3 @@ function mudouTamanho(){
             itens.style.display = 'none'
         }
 }
-
-function clickMenu(){
-    if (itens.style.display == 'block'){
-        itens.style.display= 'none'
-    }
-    else{
-        itens.style.display = 'block'
-    }
-}
